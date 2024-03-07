eba66e75a3d2cec49d75ae6456be59d4c224671e
Hashim34643 <hashimhussain7312@gmail.com>
2024-03-07 00:24:18 +0000

    reset password

--- controllers/reset-password.js
+++ controllers/reset-password.js
@@ -6,16 +6,14 @@ const resetPassword = async (req, res) => {
     const { token, newPassword } = req.body;
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const user = await User.findById(decoded.userId);
+        const hashedPassword = await bcrypt.hash(newPassword, 8);
+        const user = await User.findByIdAndUpdate(decoded.userId, { $set: { password: hashedPassword } }, { new: true });
         if (!user) {
             return res.status(404).json({ success: false, message: "User not found" });
         };
-        const hashedPassword = await bcrypt.hash(newPassword, 8);
-        user.password = hashedPassword;
-        await user.save();
+        const isMatch = await user.comparePassword(newPassword);
         res.status(200).json({ success: true, message: "Password reset successfully" });
     } catch (error) {
-        console.log(error);
         res.status(500).json({ success: false, message: "Error resetting password" });
     }
 };
