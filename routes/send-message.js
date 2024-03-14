cbfc26a4000995033f1fe08389973da44dfdd90f
Hashim34643 <hashimhussain7312@gmail.com>
2024-03-14 21:36:34 +0000

    send message testing created

--- routes/send-message.js
+++ routes/send-message.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const sendMessageController = require("../controllers/send-message");
+const isAuth = require("../middleware/auth");
 const sendMessageRouter = express.Router();
 
-sendMessageRouter.post("/:streamId/send-message", sendMessageController);
+sendMessageRouter.post("/:streamId/send-message", isAuth, sendMessageController);
 
 module.exports = sendMessageRouter;
