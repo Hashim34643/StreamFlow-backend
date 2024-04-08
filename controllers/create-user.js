9b37ce050ba0aaf4c0ca47964c91f4d688bb4d78
Hashim34643 <hashimhussain7312@gmail.com>
2024-04-08 14:35:16 +0100

    added default avatar

--- controllers/create-user.js
+++ controllers/create-user.js
@@ -3,8 +3,11 @@ const jwt = require("jsonwebtoken");
 
 const createUser = async (req, res) => {
     try {
-        const { username, firstName, lastName, email, password, confirmPassword, isStreamer } = req.body;
+        let { username, firstName, lastName, email, password, confirmPassword, isStreamer, avatar } = req.body;
         const isNewEmail = await User.isThisEmailInUse(email);
+        if (!avatar) {
+            avatar = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTs4XdD00sHtFKBYeyzKvz1CUHr598N0yrUA&s";
+        }
         if (!isNewEmail) {
             return res.status(400).json({success: false, message: "This email is already in use try sign-in"})
         }
@@ -19,11 +22,13 @@ const createUser = async (req, res) => {
             email: email.toLowerCase(),
             password,
             confirmPassword,
-            isStreamer
+            isStreamer,
+            avatar
         });
         await newUser.save();
         res.status(200).json({success: true, message: "User created successfully", userId: newUser._id });
     } catch(error) {
+        console.log(error);
         if (error.message.includes("E11000")) {
             res.status(400).json({success: false, message: "This username is already in use try sign-in"})
             return;
